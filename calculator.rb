5140c2508f940cce1b981b0b1e392936700773f7
Jacob Kopczynski <ja.kopczynski@gmail.com>
2016-01-20 15:07:25 -0800

    operations and numbers

--- calculator.rb
+++ calculator.rb
@@ -1,18 +1,43 @@
 class RPNCalculator
+  def initialize
+    @stack = []
+  end
+
+  def operation(op)
+    if @stack.length < 2
+      puts "Not Enough Arguments"
+      return
+    elsif op == :/ and @stack.last == 0
+      puts "Can't Divide By 0"
+      return
+    else
+      second = @stack.pop
+      first  = @stack.pop
+      first = first.to_f if op == :/
+      result = first.send(op,second)
+      @stack.push (result%1).zero? ? result.to_i : result
+      puts @stack.last
+    end
+  end
+
+  def number(n)
+    @stack.push (n%1).zero? ? n.to_i : n
+    puts @stack.last
+  end
 end
 
 calc = RPNCalculator.new
 while true
-    print ">"
-    token = gets
-    if token.nil?
-      print "\n"
-      break
-    elsif token.strip! == "q"
-      break
-    elsif token in %w[+ - * /]
-      calc.operation(token.to_sym)
-    elsif token == "0" or token.to_i != 0
-      calc.number(token.to_i)
-    end
+  print ">"
+  token = gets
+  if token.nil?
+    print "\n"
+    break
+  elsif token.strip! == "q"
+    break
+  elsif %w[+ - * /].include?(token)
+    calc.operation(token.to_sym)
+  elsif token == "0" or token.to_f != 0
+    calc.number(token.to_f)
+  end
 end
